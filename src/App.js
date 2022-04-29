c10d5e7299d6d3e3cd3c47c976c1af103de56164
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-04-29 13:34:18 -0500

    Agregando navegación con Link y to

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Home from './components/Home';
 import Search from './components/Search';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Venta de productos
+      <header>
+        <h1>Venta de productos</h1>
+        <nav>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/search'>search</Link></li>
+          </ul>
+        </nav>
+      </header>
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/search' element={<Search />} />
